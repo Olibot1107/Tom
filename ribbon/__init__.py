f1a54b6bde47c0c0d99893a5e80830eb7473bf4b
Olibot1107 <ojdawson1107@gmail.com>
2026-02-05 22:17:29 +0000

    hh

--- ribbon/__init__.py
+++ ribbon/__init__.py
@@ -1,1 +1,4 @@
-# Package marker
+# Package marker and convenience exports
+from .ui_terminal import TerminalUI  # noqa: F401
+from .ui_oled import OLEDDisplay  # noqa: F401
+from .weather import WeatherProvider  # noqa: F401
